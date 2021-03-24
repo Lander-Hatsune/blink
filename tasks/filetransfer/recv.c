942caf811726c81f7809709adbf175c4a8c26d2b
Lander-Hatsune <wangwwx@yeah.net>
2021-03-24 10:19:37 +0800

    feat: task file-transfer project init

---
+++ tasks/filetransfer/recv.c
@@ -0,0 +1,8 @@
+#include "quiche.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  printf("build recv\n");
+  return 0;
+}
+             
---
+++ tasks/filetransfer/send.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <ev.h>
+
+#include <sys/socket.h>
+#include "quiche.h"
+
+int main(int argv, char* argc[]) {
+  printf("build send\n");
+  return 0;
+}
+
